902fc0ef6738f269dea6fd6aa6c1b350df0096eb
Mrlong8 <xuanlong1806@gmail.com>
2026-06-24 13:03:07 +0700

    update 14

--- Chapter2and3/base-react-redux/src/App.js
+++ Chapter2and3/base-react-redux/src/App.js
@@ -1,12 +1,23 @@
 
 import './App.scss';
 import Header from './components/Header/Header'
+import { Link, Outlet } from "react-router";
+
 
 const App = () => {
   return (
     <div className="app-container">
-      Hello World
-      <Header></Header>
+      <div className='header-container'>
+        <Header></Header>
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+
+        </div>
+        <div className='app-content'>
+          <Outlet></Outlet>
+        </div>
+      </div>
     </div>
   );
 }
