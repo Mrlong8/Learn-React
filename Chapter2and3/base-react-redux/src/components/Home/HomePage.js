a5033111b94b1ec1c8dd068114e96258d5831a6f
Mrlong8 <xuanlong1806@gmail.com>
2026-06-25 13:46:15 +0700

    update 15

--- Chapter2and3/base-react-redux/src/components/Home/HomePage.js
+++ Chapter2and3/base-react-redux/src/components/Home/HomePage.js
@@ -9,7 +9,19 @@ const HomePage = (props) => {
                     type="video/mp4"
                 />
             </video>
+            <div className="homepage-content">
+                <div className="title-1">There's a better way to ask</div>
+
+                <div className="title-2">Combine AI forms and automated workflows to drive revenue growth.
+                    Run in-depth research and manage the entire customer lifecycle. All in Typeform.
+                </div>
+                <div className="title-3">
+                    <button>Get's started. It's free</button>
+                </div>
+
+            </div>
         </div>
+
     )
 }
 export default HomePage;
