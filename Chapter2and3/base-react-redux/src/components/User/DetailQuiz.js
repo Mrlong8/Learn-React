68be99eab70dbaf85e3c74a2bc2b06ca8c8950df
Mrlong8 <xuanlong1806@gmail.com>
2026-07-09 15:01:07 +0700

    update 27

--- Chapter2and3/base-react-redux/src/components/User/DetailQuiz.js
+++ Chapter2and3/base-react-redux/src/components/User/DetailQuiz.js
@@ -1,14 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from '../../services/apiService'
 import _ from 'lodash'
 import './DetailQuiz.scss'
+import Question from "./Question";
 
 const DetailQuiz = (props) => {
     const params = useParams();
     const location = useLocation();
 
     const quizId = params.id;
+
+    const [dataQuiz, setDataQuiz] = useState([])
+    const [index, setIndex] = useState(0)
 
     useEffect(() => {
         fetchQuestion();
@@ -29,30 +33,86 @@ const DetailQuiz = (props) => {
                             questionDescription = item.description;
                             image = item.image;
                         }
+                        item.answers.isSelected = false;
                         answers.push(item.answers)
                         // console.log("item answers : ", item.answers)
                     })
                     // console.log('value : ', value, ' Key : ', key)
-                    return { quizId: key, answers, questionDescription, image }
+                    return { questionId: key, answers, questionDescription, image }
                 }
                 )
                 .value()
-            console.log(data)
+            console.log(data);
+            setDataQuiz(data);
         }
     }
 
     // console.log("Check params : ", params)
+    const handlePrev = () => {
+        if (index - 1 < 0)
+            return;
+        setIndex(index - 1)
+    }
+
+    const handleNext = () => {
+        if (dataQuiz && dataQuiz.length > (index + 1))
+            setIndex(index + 1)
+    }
+
+    const handleFinish = () => {
+
+    }
+
+    const handleCheckBox = (answerId, questionId) => {
+        let dataQuizClone = _.cloneDeep(dataQuiz);
+        let question = dataQuizClone.find(item => +item.questionId === +questionId)
+        if (question && question.answers) {
+            let b = question.answers.map(item => {
+                if (+item.id === +answerId) {
+                    item.isSelected = !item.isSelected;
+                }
+                return item
+            })
+            console.log(b)
+            question.answers = b;
+
+        }
+        let index = dataQuizClone.findIndex(item => +item.questionId === +questionId)
+        if (index > -1) {
+            dataQuizClone[index] = question;
+            setDataQuiz(dataQuizClone);
+        }
+
+    }
+
     return (
         <div className='detail-quiz-container'>
             <div className='left-content'>
                 <div className='title'>
-                    {location?.state?.quizTitle}
+                    Quiz {quizId}: {location?.state?.quizTitle}
                 </div>
+                <hr />
                 <div className='q-body'>
                     <img src="" alt="" />
                 </div>
                 <div className='q-content'>
-                    questins content
+                    <Question
+                        handleCheckBox={handleCheckBox}
+                        index={index}
+                        data={dataQuiz && dataQuiz.length > 0
+                            ? dataQuiz[index] : []}
+                    />
+                </div>
+                <div className='footer'>
+                    <button className='btn btn-secondary'
+                        onClick={() => handlePrev()}
+                    >Prev</button>
+                    <button className='btn btn-primary'
+                        onClick={() => handleNext()}
+                    >Next</button>
+                    <button className='btn btn-warning'
+                        onClick={() => handleFinish()}
+                    >Finish</button>
                 </div>
             </div>
             <div className='right-content'>
